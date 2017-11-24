51df658b8bc6ff4add020af6b039eec6a2cb338c
JosPerez <jos.perez.mtz@gmail.com>
2017-11-24 16:23:05 -0600

    Primera parte

---
+++ appBio/App.js
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { AppRegistry,StyleSheet, Text, View, Button, } from 'react-native';
+
+export default class App extends React.Component {
+  
+
+
+  render() {
+    return (
+      <View style={styles.conteiner}>
+      <Text style={styles.title}>Biohacking App</Text>
+      <
+      </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  title:{
+    fontSize:30,
+    margin:10,
+    textAlign:'center',
+    fontWeight: 'bold',
+    color: 'blue',
+  },
+  conteiner :{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+});
